f890fcba5636ae3fee32fce87b834f5c4ad0a561
paul.mason <paul.imperialroad@gmail.com>
2018-09-21 20:25:15 +0100

    updates for prettier code

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ const router = require('./route.js');
 const handlebars = require('express-handlebars');
 const path = require('path');
 
-app.set('views','views');
+app.set('views', 'views');
 app.set('view engine', 'hbs');
-app.engine('hbs', handlebars({defaultLayout: 'main',
-                            partialsDir:"views/partials/",
-                            extname:".hbs"}));
+app.engine('hbs', handlebars({
+	defaultLayout: 'main',
+	partialsDir: "views/partials/",
+	extname: ".hbs"
+}));
 app.use(express.static('public'));
 
 app.use(router);
 
-app.listen(8000, function(){
-    console.log(chalk.green('listening on port 8000...'));
+app.listen(8000, function () {
+	console.log(chalk.green('listening on port 8000...'));
 })
